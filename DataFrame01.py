cef60842ba9752822723f9b5302dd27d5727e275
MaduPereira <dudasilvapereira@outlook.com.br>
2023-10-08 13:09:04 -0300

    Update DataFrame01.py

--- DataFrame01.py
+++ DataFrame01.py
@@ -1,29 +1,28 @@
 
-url = 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SGL/2022/uf=BA/lote=1/part-00000-ecd4fc79-1113-4c73-9917-0f83aa9e0235.c000.csv'
+# url = 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SGL/2022/uf=BA/lote=1/part-00000-ecd4fc79-1113-4c73-9917-0f83aa9e0235.c000.csv'
+# dtypes = {
+#     "sintomas":"str",
+#     "profissionalSaude": "str",
+#     "racaCor": "str",
+#     "outrosSintomas": "str",
+#     "idade": "float",
+#     "sexo": "str",  #tipo de dados apropriado
+# }
+# arquivo = "/Users/Eduarda/OneDrive/Área de Trabalho/PIBITI-IFBA-CNPq-2023-2024/part-00000-7794e106-1591-454b-abb8-7840c03edf5e.c000.csv"
 
-#df = requests.get(url)
+# df = pd.read_csv(url, dtype=dtypes, sep=';', on_bad_lines='skip')
+# #print(df)
+# #df = requests.get(url)
 
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
 
 # Carregue seu DataFrame aqui
-# ...
-dtypes = {
-    "sintomas":"str",
-    "profissionalSaude": "str",
-    "racaCor": "str",
-    "outrosSintomas": "str",
-    "idade": "float",
-    "sexo": "str",  # Ou o tipo de dados apropriado
-    # Adicione outras colunas aqui se necessário
-}
-arquivo = "/Users/Eduarda/OneDrive/Área de Trabalho/PIBITI-IFBA-CNPq-2023-2024/part-00000-7794e106-1591-454b-abb8-7840c03edf5e.c000.csv"
+url = 'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SGL/2022/uf=BA/lote=1/part-00000-ecd4fc79-1113-4c73-9917-0f83aa9e0235.c000.csv'
+df = pd.read_csv(url, sep=';')
 
-df = pd.read_csv(url, dtype=dtypes, sep=';', on_bad_lines='skip')
-#print(df)
-
-#Função para atualizar a tabela com base nos filtros
+# Função para atualizar a tabela com base nos filtros
 def atualizar_tabela():
     sintomas_selecionados = combo_sintomas.get()
     profissional_saude_selecionado = combo_profissional_saude.get()
@@ -49,34 +48,60 @@ def atualizar_tabela():
         for index, row in resultados_filtrados.iterrows():
             tree.insert("", "end", values=(row['sintomas'], row['profissionalSaude'], row['racaCor'], row['idade']))
 
+# Função para limpar filtros e mostrar a tabela completa
+def limpar_filtros():
+    # Resetar valores dos ComboBoxes
+    combo_sintomas.set("Selecione um sintoma")
+    combo_profissional_saude.set("Selecione um profissional de saúde")
+    combo_racaCor.set("Selecione uma Raça/Cor")
+    combo_idade.set("Selecione uma Idade")
+    
+    # Limpar a tabela existente
+    for i in tree.get_children():
+        tree.delete(i)
+
+    # Preencher a tabela com todos os dados
+    for index, row in df.iterrows():
+        tree.insert("", "end", values=(row['sintomas'], row['profissionalSaude'], row['racaCor'], row['idade']))
+
 # Crie uma janela principal
 root = tk.Tk()
 root.title("Filtragem de Tabela")
 
+# Divida os valores na coluna 'sintomas' com base nas vírgulas
+df['sintomas'] = df['sintomas'].str.split(',')
+df['profissionalSaude'] = df['profissionalSaude'].str.split(',')
+df['racaCor'] = df['racaCor'].str.split(',')
+
+# Use a função explode para criar várias linhas a partir de uma única linha
+df = df.explode('sintomas')
+df = df.explode('profissionalSaude')
+df = df.explode('racaCor')
+
 # Crie as ComboBox para seleção de filtros
-sintomas = df['sintomas'].unique()
-combo_sintomas = ttk.Combobox(root, values=sintomas, state="readonly")
+combo_sintomas = ttk.Combobox(root, state="readonly")
 combo_sintomas.set("Selecione um sintoma")
 combo_sintomas.grid(row=0, column=0, sticky="e")
 
-profissionais_saude = df['profissionalSaude'].unique()
-combo_profissional_saude = ttk.Combobox(root, values=profissionais_saude, state="readonly")
+combo_profissional_saude = ttk.Combobox(root, state="readonly")
 combo_profissional_saude.set("Selecione um profissional de saúde")
 combo_profissional_saude.grid(row=0, column=1, sticky="e")
 
-racaCor = df['racaCor'].unique()
-combo_racaCor = ttk.Combobox(root, values=racaCor, state="readonly")
+combo_racaCor = ttk.Combobox(root, state="readonly")
 combo_racaCor.set("Selecione uma Raça/Cor")
 combo_racaCor.grid(row=0, column=3, sticky="w")
 
-idade = df['idade'].unique()
-combo_idade = ttk.Combobox(root, values=idade, state="readonly")
+combo_idade = ttk.Combobox(root, state="readonly")
 combo_idade.set("Selecione uma Idade")
 combo_idade.grid(row=0, column=4, sticky="w")
 
 # Botão para aplicar filtros
 botao_filtrar = ttk.Button(root, text="Filtrar", command=atualizar_tabela)
-botao_filtrar.grid(row=0, column=4)
+botao_filtrar.grid(row=0, column=5, sticky="e")
+
+# Botão para limpar filtros
+botao_limpar = ttk.Button(root, text="Limpar Filtros", command=limpar_filtros)
+botao_limpar.grid(row=0, column=6, sticky="e")
 
 # Crie uma tabela para exibir os resultados
 columns = ("Sintomas", "Profissional de Saúde", "Raça/Cor", "Idade")
